f310ea71f4ab594e55d4133a118aa93788c46abe
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-07-20 19:18:11 +0700

    Implement exercise 1.36

---
+++ chapter-1/exercise-1.36.rkt
@@ -0,0 +1,31 @@
+; Exercise 1.36
+;
+; Modify `fixed-point` so that it prints the sequence of approximations it
+; generates, using the `newline` and display primitives shown in 1.22. Then find
+; a solution to /x^x = 1000/ by finding a fixed point of /x |->
+; log(1000)/log(x)/. (Use Scheme's primitive `log` procedure which computes
+; natural logarithms.)
+;
+; Compare the number of steps this takes with and without average damping.
+;
+; (Note that you cannot start `fixed-point` with a guess of 1, as this would
+; cause division by log(1) = 0)
+
+#lang sicp
+
+(define tolerance 0.00001)
+(define (fixed-point function
+                     first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (function guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
