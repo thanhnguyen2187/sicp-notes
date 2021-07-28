6d3b1c34d607a9a865f442b1264bb0a6ce431589
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-07-28 16:33:05 +0700

    Implement exercise 1.42

---
+++ chapter-1/exercise-1.42.rkt
@@ -0,0 +1,17 @@
+; Exercise 1.42
+;
+; Let /f/ and /g/ be two one-argument functions. The /composition f/ after /g/
+; is defined to be the function x |-> f(g(x)). Define a procedure `compose` that
+; implements composition. For example, if `inc` is a procedure that adds 1 to
+; its argument,
+;
+; ...
+
+#lang sicp
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (square x) (* x x))
+
+((compose square inc) 6)
