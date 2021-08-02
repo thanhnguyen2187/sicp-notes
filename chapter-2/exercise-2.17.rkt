57ed4c0e10fb6d84dbf8d4b63dca4f416e8472da
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-08-02 22:53:15 +0700

    Implement exercise 2.17

---
+++ chapter-2/exercise-2.17.rkt
@@ -0,0 +1,15 @@
+; Exercise 2.17
+;
+; Define a procedure `last-pair` that returns the list that contains only the
+; last element of a given (nonempty) list:
+;
+; ...
+
+#lang sicp
+
+(define (last-pair l)
+  (if (null? (cdr l))
+      (car l)
+      (last-pair (cdr l))))
+
+(last-pair (list 1 2 3 4 5 6))
