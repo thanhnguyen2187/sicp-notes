e6b020b6cc32ca0b9b7ad18e3130efa304b9a786
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-08-02 22:53:57 +0700

    Change functions' order exercise 2.8

--- chapter-2/exercise-2.8.rkt
+++ chapter-2/exercise-2.8.rkt
@@ -20,13 +20,12 @@
   (mul-interval x
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))))
-
-(define (make-interval a b) (cons a b))
-(define (lower-bound x) (car x))
-(define (upper-bound x) (cdr x))
-
 (define (sub-interval x y)
   (make-interval (- (lower-bound x)
                     (upper-bound y))
                  (- (upper-bound x)
                     (lower-bound y))))
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
