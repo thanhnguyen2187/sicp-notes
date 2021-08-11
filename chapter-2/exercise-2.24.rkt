1c17588510ea780ebdae3bcbfb553966638cc48e
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-08-11 20:50:59 +0700

    Implement exercise 2.24

---
+++ chapter-2/exercise-2.24.rkt
@@ -0,0 +1,9 @@
+; Exercise 2.24
+;
+; Suppose we evaluate the expression `(list 1 (list 2 (list 3 4)))`. Give the
+; result printed by the interpreter, the corresponding box-and-pointer
+; structure, and the iterpretation of this as a tree.
+
+#lang sicp
+
+(list 1 (list 2 (list 3 4)))
