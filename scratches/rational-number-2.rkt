1ef60faa59a5a505263e75f19844d8a50c6441dd
Thanh Nguyen <thanhnguyen2187@gmail>
2021-08-24 21:09:33 +0700

    Write scratches on rational number 2

---
+++ scratches/rational-number-2.rkt
@@ -0,0 +1,35 @@
+; Exercise ?.?
+
+#lang sicp
+
+(define (apply-generic) 0)
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+(define (attach-tag) 0)
+(define (put) 0)
+(define (get) 0)
+
+(define (install-scheme-number-package)
+  (define (tag x) (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  'done)
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
+
+(define (install-rational-package)
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d))
+  'done
+  )
+
