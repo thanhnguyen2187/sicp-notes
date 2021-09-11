af124db960bc08a0cb8c2d8336b84eefef01439c
Thanh Nguyen <thanhnguyen2187@gmail>
2021-09-11 19:32:35 +0700

    Implement exercise 2.92

---
+++ chapter-2/exercise-2.92.rkt
@@ -0,0 +1,7 @@
+; Exercise 2.92
+;
+; By imposing an ordering on variables, extend the polynomial package so that
+; addition and multiplication of polynomial works for polynomials in different
+; variables.
+
+#lang sicp
