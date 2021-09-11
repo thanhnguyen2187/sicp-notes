36cc496eaf567ff5036f9c97e424a24a778ec7a4
Thanh Nguyen <thanhnguyen2187@gmail>
2021-09-11 19:33:48 +0700

    Implement exercise 3.8

---
+++ chapter-3/exercise-3.8.rkt
@@ -0,0 +1,28 @@
+; Exercise 3.8
+;
+; When we defined the evaluation model in Section 1.1.3, we said that the first
+; step in evaluating an expression is to evaluate its subexpressions. But we
+; never specified the order in which the subexpressions should be evaluated
+; (e.g., left to right or right to left). When we introduce assignment, the
+; order in which the arguments to a procedure are evaluated can make a
+; difference to the result.
+;
+; Define a simple procedure f such that evaluating
+;
+; ...
+;
+; Will return 0 if the argument to `+` are evaluated from left to right, but
+; will return 1 if the arguments are evaluated from right to left.
+
+#lang sicp
+
+(define y 0)
+(define (f x)
+  (define old-y y)
+  (set! y x)
+  old-y)
+
+; > (+ (f 0) (f 1))
+; 0
+; > (+ (f 1) (f 0))
+; 1
