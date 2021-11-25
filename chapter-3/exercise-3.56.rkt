a08aa3662e89b16250ab7e67d0b65d513902f803
Thanh Nguyen <thanhnguyen2187@gmail>
2021-11-25 20:13:09 +0700

    Implement exercise 3.56

---
+++ chapter-3/exercise-3.56.rkt
@@ -0,0 +1,139 @@
+; Exercise 3.56
+;
+; A famous problem, first raised by R. Hamming, is to enumerate, in ascending
+; order with no repetitions, all positive integers with no prime factors other
+; than 2, 3, or 5.
+;
+; One obivious way to do this is to simply test each integer in turn to see
+; whether it has any factors other than 2, 3, and 5. But this is very
+; inefficient, since, as the integers get larget, fewer and fewer of them fit
+; the requirement.
+;
+; As an alternative, let us call the required stream of numbers `S` and notice
+; the following facts about it.
+;
+; * `S` begins with 1.
+; * The elements of `(scale-stream S 2)` are also elements of S.
+; * The same is true for `(scale-stream S 3)`, and `(scale-stream S 5)`.
+; * These are all elements of `S`.
+;
+; Now all we have to do is combine elements from these resources. For this we
+; define a procedure `merge` that combines two ordered streams into one ordered
+; result stream, eliminating repetitions:
+;
+; ...
+
+#lang sicp
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a d) (cons a (delay d)))))
+
+(define (stream-car stream)
+  (car stream))
+
+(define (stream-cdr stream)
+  (force (cdr stream)))
+
+(define (stream-null? stream)
+  (null? stream))
+
+(define the-empty-stream '())
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+        low
+        (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s)
+                  (- n 1))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream (apply proc (map stream-car argstreams))
+                   (apply stream-map
+                          (cons proc (map stream-cdr argstreams))))))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define (display-line x)
+  (display x) (newline))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+          (begin (set! result (proc))
+                 (set! already-run? true)
+                 result)
+          result))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers
+  (integers-starting-from 1))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(define (partial-sums stream)
+  (add-streams stream
+               (cons-stream 0 (partial-sums stream))))
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+          (let ((s1car (stream-car s1))
+                (s2car (stream-car s2)))
+            (cond ((< s1car s2car)
+                   (cons-stream s1car
+                                (merge (stream-cdr s1) s2)))
+                  ((> s1car s2car)
+                   (cons-stream s2car
+                                (merge (stream-cdr s2) s1)))
+                  (else (cons-stream s1car
+                                     (merge (stream-cdr s1)
+                                            (stream-cdr s2)))))))))
+
+(define S (cons-stream 1 (merge (scale-stream integers 2)
+                                (merge (scale-stream integers 3)
+                                       (scale-stream integers 5)))))
+
+(stream-ref S 0)
+(stream-ref S 1)
+(stream-ref S 2)
+(stream-ref S 3)
+(stream-ref S 4)
+(stream-ref S 5)
+(stream-ref S 6)
+(stream-ref S 7)
+(stream-ref S 8)
